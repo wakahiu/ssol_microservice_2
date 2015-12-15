445ed4d78cdd794f7900a8a34f624490ce547c47
wakahiu <wakahiu@berkeley.edu>
2015-12-15 12:32:02 -0500

    Removed all the sh*t we do not need

--- gateway/routes.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(app){
-	var router = require('./controllers/router');
-	app.get('/students/:id', router.getStudent);
-}
--- students_service/config/env/development.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-	port: process.env.PORT || 3003,
-	db: 'mongodb://0.0.0.0/students_service_3',
-	starting_uni: 'q',
-	ending_uni: 'z'
-};
